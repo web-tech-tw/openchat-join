e387e2d0b5dd4b45df45c8c075da993d630128ac
不知火 Shiranui <supersonic@livemail.tw>
2024-10-14 15:02:32 +0800

    feat: upgrade to sara v3

--- src/init/const.js
+++ src/init/const.js
@@ -2,4 +2,13 @@
 // Constants
 
 exports.APP_NAME = "openchat-join";
+exports.APP_DESCRIPTION = "A tiny but powerful microservice framework.";
+
+exports.APP_VERSION = "latest";
+
+exports.APP_AUTHOR_NAME = "Taiwan Web Technology Promotion Organization";
+exports.APP_AUTHOR_URL = "https://web-tech.tw";
+
+exports.OPENAPI_EXPORTED_FILENAME = "openapi_exported.json";
+exports.PUBLIC_KEY_FILENAME = "keypair_public.pem";
 exports.SECRET_FILENAME = "secret.key";
